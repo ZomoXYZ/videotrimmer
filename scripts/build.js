7ad02c1ca7e142bd2aae00ce60cd8ea2a15e61b3
ashley zomo <ashleyzomo@gmail.com>
2022-02-22 15:40:19 -0600

    build scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,20 @@
 const { build } = require('esbuild')
 
+// API
+build({
 
+    entryPoints: ['src/API/main.ts'],
+
+    target: 'node16',
+    platform: 'node',
+
+    outdir: 'dist/API',
+
+    bundle: true,
+
+});
+
+// UI
 build({
 
     entryPoints: ['src/UI/App.tsx'],
@@ -9,4 +23,4 @@ build({
 
     bundle: true,
 
-})
+});
