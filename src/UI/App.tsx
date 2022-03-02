416e6b571a588f1d093f19d09eeb21dfe124dd19
ashley zomo <ashleyzomo@gmail.com>
2022-03-02 14:24:04 -0600

    fixed router

--- src/UI/App.tsx
+++ src/UI/App.tsx
@@ -1,58 +1,29 @@
-import { render, h, Fragment } from 'preact';
+import { render } from 'preact';
 import Router from 'preact-router';
 import Main from './pages/main';
 import Error from './pages/error';
 import Editor from './pages/editor';
 import Progress from './pages/progress';
 import Settings from './pages/settings';
-import Tab from './components/tabs';
-import { useContext } from 'preact/hooks';
+import { createHashHistory } from 'history';
 
 import './App.css';
-import Button from './components/button';
 
 function App() {
-    const { changeTab } = useContext(Tab.Context);
-
-    return (
-        <Fragment>
-            <Tab.Provider>
-                <Tab.Content name="home">
-                    home
-                    <Button onClick={() => {
-                        let err = changeTab('test');
-                        if (err) {
-                            console.error(err)
-                        }
-                    }}>
-                        go to test page
-                    </Button>
-                </Tab.Content>
-                <Tab.Content name="test">
-                    test
-                    <Button onClick={() => {
-                        let err = changeTab('home');
-                        if (err) {
-                            console.error(err)
-                        }
-                    }}>
-                        go to home page
-                    </Button>
-                </Tab.Content>
-                <Tab.Slot/>
-            </Tab.Provider>
-        </Fragment>
-    );
-    /*
     
-        <Router>
-            <Main path="/"/>
+    return (
+        // https://github.com/preactjs/preact-router/issues/423
+        // the typemismatch is a lie apparently
+        // @ts-ignore
+        <Router history={createHashHistory()}>
+            <Main path="/" />
             <Error path="/error"/>
             <Editor path="/editor"/>
             <Progress path="/progress"/>
             <Settings path="/settings"/>
         </Router>
-    */
+    );
+    
 }
 
 render(<App />, document.body);
