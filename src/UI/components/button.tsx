4fa6936ff2294c3d235bb034559c14f1c68d85e8
ashley zomo <ashleyzomo@gmail.com>
2022-03-07 13:08:09 -0600

    updated button's theme import

--- src/UI/components/button.tsx
+++ src/UI/components/button.tsx
@@ -1,26 +1,29 @@
 import { ComponentProps } from '../types/component';
 import { css } from '@emotion/css'
-import Theme from '../styles/theme';
+import { getColor } from '../styles/theme';
 
 type LabelProps = ComponentProps<HTMLLabelElement> & {
 	type?: "button" | "file";
 };
 
 export default function Button({ children, type = "button", ...props }: LabelProps) {
+
+	const Color = getColor();
+
 	return (
 		<label class={css`
 		margin-top: 3px;
 		padding: 6px 8px;
 		border-radius: 4px;
 		
-		background: ${Theme.color['dark'].button.background};
-		border: 1px solid ${Theme.color['dark'].button.border};
+		background: ${Color.button.background};
+		border: 1px solid ${Color.button.border};
 
 		&:hover {
-			background: ${Theme.color['dark'].button.backgroundHover};
+			background: ${Color.button.backgroundHover};
 		}
 		&:active {
-			background: ${Theme.color['dark'].button.backgroundActive};
+			background: ${Color.button.backgroundActive};
 		}
 		`} {...props}>
 			<input class={css`
